4feece9a32b58740a0fa049d8afab50b53b3f862
ginoscott <ginoscottdev@gmail.com>
2022-11-22 15:47:53 -1000

    solving exhaustive deps for refs

--- adcs-plugin/src/SimplePanel.tsx
+++ adcs-plugin/src/SimplePanel.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react'
 import { PanelProps } from '@grafana/data';
 import { InlineFieldRow, Input, Select } from '@grafana/ui';
 import { useCosmosTimeline, useDomUpdate } from 'helpers/hooks';
-import { SimpleOptions, TimeEvent } from 'types';
+import { SimpleOptions } from 'types';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 interface Props extends PanelProps<SimpleOptions> {}
@@ -39,26 +39,19 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height, eve
   const refWebGLContainer = useRef<HTMLDivElement>(null);
   //const refId = useRef<number>(0);
   //const [renderer, setRenderer] = useState<THREE.WebGLRenderer>();
-  const refRenderer = useRef<THREE.WebGLRenderer>();
-  const refScene = useRef<THREE.Scene>();
-  const refCamera = useRef<THREE.OrthographicCamera>();
-  const refModel = useRef<THREE.Group>();
   // An array of references to the text boxes
   // const refInputs = useRef<RefDict>({});
   // The index into the data array
   //const refIdxs = useRef<number[]>([]);
-  const [refInputs, updateDOMRefs] = useDomUpdate(data);
+  const [
+    refRenderer,
+    refScene,
+    refCamera,
+    refModel,
+    refInputs,
+    updateDOMRefs
+  ] = useDomUpdate(data);
   useCosmosTimeline(data, eventBus, updateDOMRefs);
-
-  useEffect(() => {
-    // Clean up renderer on unmount
-    return () => {
-      if (refRenderer.current !== undefined) {
-        refRenderer.current.dispose();
-        refRenderer.current = undefined;
-      }
-    }
-  }, []);
 
   // Setup scene
   useEffect(() => {
@@ -67,10 +60,10 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height, eve
     if (!container) {
       return;
     }
-    if (refRenderer.current === undefined) {
+    if (refRenderer.current === undefined || refRenderer.current === null) {
       const renderer = new THREE.WebGLRenderer();
       // Insert into div
-      document.body.appendChild(renderer.domElement );
+      document.body.appendChild(renderer.domElement);
       container.appendChild(renderer.domElement);
       refRenderer.current = renderer;
       //setRenderer(renderer);
@@ -96,7 +89,7 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height, eve
     const directionalLight = new THREE.DirectionalLight(0xeeeeee, 3.5);
     directionalLight.position.set(1, 4, 2);
     scene.add(directionalLight);
-    scene.background= new THREE.Color(0x050505);
+    scene.background = new THREE.Color(0x050505);
 
     // Add axis arrows
     const origin = new THREE.Vector3(0,0,0);
@@ -117,98 +110,8 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height, eve
       refModel.current = obj;
       refRenderer.current!.render(scene, camera);
     });
-  }, [width, height]);
-
-  // ---------------------------------------------------
-  // useEffect(() => {
-  //   // Array of references
-  //   // Number of columns is the total -1 to exclude the time column
-  //   const numColumns = data.series[0].fields.length-1;
-  //   // Array of indices
-  //   if (refIdxs.current.length < numColumns) {
-  //     for (let i = refIdxs.current.length; i < numColumns; i++) {
-  //       refIdxs.current.push(0);
-  //     }
-  //   } else {
-  //     refIdxs.current = refIdxs.current.slice(0, numColumns);
-  //   }
-  //   console.log('numColumns:', numColumns, 'refIdxs.length:', refIdxs.current.length);
-  // }, [data]);
-  // Imperative animation call
-  // const updateDOMRefs = useCallback((data: PanelData, event: TimeEvent) => {
-  //   if (
-  //     !data.series.length || // Check if there is valid query result
-  //     data.series[0].fields.length < 2// || // Check if there are time and value columns in query
-  //     //refIdx.current >= data.series[0].fields[0].values.length // Check if there are values in those columns
-  //   ) {
-  //     //refIdx.current = 0;
-  //     return;
-  //   }
-  //   Object.entries(refInputs.current).forEach(([key, ref], i) => {
-  //     if (ref !== null) {
-  //       // Check that there are query results
-  //       if (!data.series.length) {
-  //         return;
-  //       }
-  //       // setState takes one rerender cycle to be reset to the correct value
-  //       if (i+1 >= data.series[0].fields.length) {
-  //         return;
-  //       }
-  //       // Query must have returned some values
-  //       const timeValues = data.series[0].fields[0].values;
-  //       if (timeValues.length === 0) {
-  //         return;
-  //       }
-  //       // If index is out of bounds, set it back to the start
-  //       if (refIdxs.current[i] >= timeValues.length-1) {
-  //         refIdxs.current[i] = 0;
-  //       }
-  //       // If new timestamp is less than our current timestamp, then search from start
-  //       // TODO: depending on circumstances, we could perhaps just search backwards, eg: if scrubbing is in event?
-  //       let time = timeValues.get(refIdxs.current[i]);
-  //       if (time > event.payload.time!) {
-  //         refIdxs.current[i] = 0;
-  //       }
-  //       // Search through timestamps, and get the timestamp that is one before we go over the event timestamp
-  //       for (; refIdxs.current[i] < timeValues.length-1; refIdxs.current[i]++) {
-  //         time = timeValues.get(refIdxs.current[i]);
-  //         if (time === event.payload.time!) {
-  //           break;
-  //         }
-  //         if (time > event.payload.time!) {
-  //           refIdxs.current[i] -= 1;
-  //           break;
-  //         }
-  //       }
-  //       const currentTemp: string = (data.series[0].fields[i+1].values.get(refIdxs.current[i]) ?? 0);
-  //       ref.value = currentTemp;
-  //     }
-  //   });
-  // }, []);
-
-  // ---------------------------------------------------
-  // Imperative animation controller
-  // Unix seconds timestamp that denotes current time, obtained from cosmos-timeline event publisher
-  useEffect(() => {
-    const subscriber = eventBus.getStream(TimeEvent).subscribe(event => {
-      if (event.payload.time !== undefined) {
-        updateDOMRefs(data, event);
-        requestAnimationFrame(() => {
-          if (refModel.current !== undefined && refRenderer.current !== undefined && refScene.current !== undefined && refCamera.current !== undefined)
-          {
-            refModel.current.rotation.x += 0.1;
-            refRenderer.current.render(refScene.current, refCamera.current);
-          }
-        });
-      }
-    });
-
-    return () => {
-      subscriber.unsubscribe();
-    }
-  }, [eventBus, data, updateDOMRefs]);
-
-  // ---------------------------------------------------
+    // Note: refs are stable, will not trigger effect, but calms the exhaustive-deps lint rule
+  }, [width, height, refRenderer, refScene, refCamera, refModel, refInputs]);
 
   return (
     <div>
--- adcs-plugin/src/helpers/hooks.ts
+++ adcs-plugin/src/helpers/hooks.ts
@@ -2,47 +2,58 @@ import { useCallback, useEffect, useRef } from 'react';
 import { EventBus, PanelData } from '@grafana/data';
 import { RefDict, TimeEvent, TimeEventCallback } from '../types';
 
+// Hook to listen to eventBus for cosmos timeevents, running animation callback when event fires
 export const useCosmosTimeline = (data: PanelData, eventBus: EventBus, callback: TimeEventCallback) => {
-    // Imperative animation controller
+  // ---------------------------------------------------
+  // Imperative animation controller
   // Unix seconds timestamp that denotes current time, obtained from cosmos-timeline event publisher
   useEffect(() => {
     const subscriber = eventBus.getStream(TimeEvent).subscribe(event => {
       if (event.payload.time !== undefined) {
         callback(data, event);
-        // requestAnimationFrame(() => {
-        //   if (refModel.current !== undefined && refRenderer.current !== undefined && refScene.current !== undefined && refCamera.current !== undefined)
-        //   {
-        //     refModel.current.rotation.x += 0.1;
-        //     refRenderer.current.render(refScene.current, refCamera.current);
-        //   }
-        // });
-        console.log('event fired');
       }
     });
     return () => {
-      console.log('usecosmostimeline return');
       subscriber.unsubscribe();
     }
   }, [eventBus, data, callback]);
 
 };
 
-type DomUpdateReturn = [React.MutableRefObject<RefDict>, (data: PanelData, event: TimeEvent) => void];
 
-// Imperative animation call
+// ---------------------------------------------------
+type DomUpdateReturn = [
+  refRenderer: React.MutableRefObject<THREE.WebGLRenderer | undefined>,
+  refScene: React.MutableRefObject<THREE.Scene | undefined>,
+  refCamera: React.MutableRefObject<THREE.OrthographicCamera | undefined>,
+  refModel: React.MutableRefObject<THREE.Group | undefined>,
+  refInputs: React.MutableRefObject<RefDict>,
+  callback: (data: PanelData, event: TimeEvent) => void
+];
+// Imperative animation manager
 export const useDomUpdate = (data: PanelData): DomUpdateReturn => {
-  // const refWebGLContainer = useRef<HTMLDivElement>(null);
-  // //const refId = useRef<number>(0);
-  // //const [renderer, setRenderer] = useState<THREE.WebGLRenderer>();
-  // const refRenderer = useRef<THREE.WebGLRenderer>();
-  // const refScene = useRef<THREE.Scene>();
-  // const refCamera = useRef<THREE.OrthographicCamera>();
-  // const refModel = useRef<THREE.Group>();
+  //const refId = useRef<number>(0);
+  //const [renderer, setRenderer] = useState<THREE.WebGLRenderer>();
+  const refRenderer = useRef<THREE.WebGLRenderer>();
+  const refScene = useRef<THREE.Scene>();
+  const refCamera = useRef<THREE.OrthographicCamera>();
+  const refModel = useRef<THREE.Group>();
   // An array of references to the text boxes
   const refInputs = useRef<RefDict>({});
   // The index into the data array
   const refIdxs = useRef<number[]>([]);
 
+  useEffect(() => {
+    // Clean up renderer on unmount
+    return () => {
+      if (refRenderer.current !== undefined) {
+        refRenderer.current.dispose();
+        refRenderer.current = undefined;
+      }
+    }
+  }, []);
+
+  // Update refIdxs
   useEffect(() => {
     // Array of references
     // Number of columns is the total -1 to exclude the time column
@@ -57,6 +68,8 @@ export const useDomUpdate = (data: PanelData): DomUpdateReturn => {
     }
   }, [data]);
 
+  // ---------------------------------------------------
+  // Imperative animation update call
   const updateDOMRefs = useCallback((data: PanelData, event: TimeEvent) => {
     if (
       !data.series.length || // Check if there is valid query result
@@ -66,6 +79,11 @@ export const useDomUpdate = (data: PanelData): DomUpdateReturn => {
       //refIdx.current = 0;
       return;
     }
+    let yaw = 0;
+    let pitch = 0;
+    let roll = 0;
+
+    // Update field values
     Object.entries(refInputs.current).forEach(([key, ref], i) => {
       if (ref !== null) {
         // Check that there are query results
@@ -102,10 +120,32 @@ export const useDomUpdate = (data: PanelData): DomUpdateReturn => {
             break;
           }
         }
-        const currentValue: string = (data.series[0].fields[i+1].values.get(refIdxs.current[i]) ?? 0);
-        ref.value = currentValue;
+        const currentValue: number = (data.series[0].fields[i+1].values.get(refIdxs.current[i]) ?? 0);
+        ref.value = currentValue.toString();
+        switch(key)
+        {
+          case 'YAW':
+            yaw = currentValue;
+            break;
+          case 'PITCH':
+            pitch = currentValue;
+            break;
+          case 'ROLL':
+            roll = currentValue;
+            break;
+        }
+      }
+    });
+
+    // Update threejs model rotation
+    requestAnimationFrame(() => {
+      if (refModel.current !== undefined && refRenderer.current !== undefined && refScene.current !== undefined && refCamera.current !== undefined)
+      {
+        refModel.current.rotation.set(yaw, pitch, roll);
+        refRenderer.current.render(refScene.current, refCamera.current);
       }
     });
   }, []);
-  return [refInputs, updateDOMRefs];
+
+  return [refRenderer, refScene, refCamera, refModel, refInputs, updateDOMRefs];
 };
