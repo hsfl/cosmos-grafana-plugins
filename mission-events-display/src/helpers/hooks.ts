ea69fc91cb2e6e1350a06568f3ec8f0696f27bde
ginoscott <ginoscottdev@gmail.com>
2022-12-28 16:39:54 -1000

    adding this too, fix later if broken

---
+++ mission-events-display/src/helpers/hooks.ts
@@ -0,0 +1,53 @@
+import { useCallback, useEffect, useRef } from 'react';
+import { EventBus } from '@grafana/data';
+import { TimeEvent, TimeEventCallback } from '../types';
+
+// Hook to listen to eventBus for cosmos timeevents, running animation callback when event fires
+export const useCosmosTimeline = (eventBus: EventBus, callback: TimeEventCallback) => {
+  // ---------------------------------------------------
+  // Imperative animation controller
+  // Unix seconds timestamp that denotes current time, obtained from cosmos-timeline event publisher
+  useEffect(() => {
+    const subscriber = eventBus.getStream(TimeEvent).subscribe((event) => {
+      if (event.payload.time !== undefined) {
+        callback(event);
+      }
+    });
+    return () => {
+      subscriber.unsubscribe();
+    };
+  }, [eventBus, callback]);
+};
+
+// ---------------------------------------------------
+type DomUpdateReturn = [
+  regTimeTickGroup: React.MutableRefObject<SVGElement | null>,
+  callback: (event: TimeEvent) => void
+];
+// Imperative animation manager
+export const useDomUpdate = (): DomUpdateReturn => {
+  //const refId = useRef<number>(0);
+  //const [renderer, setRenderer] = useState<THREE.WebGLRenderer>();
+  const refTimeTickGroup = useRef<SVGElement | null>(null);
+
+  useEffect(() => {
+    // Clean up renderer on unmount
+    return () => {
+
+    };
+  }, []);
+
+  // ---------------------------------------------------
+  // Imperative animation update call
+  const updateDOMRefs = useCallback((event: TimeEvent) => {
+
+    // Update threejs model rotation
+    requestAnimationFrame(() => {
+      if (refTimeTickGroup.current !== undefined) {
+        // console.log('reftimetickgroup', refTimeTickGroup.current.top);
+      }
+    });
+  }, []);
+
+  return [refTimeTickGroup, updateDOMRefs];
+};
