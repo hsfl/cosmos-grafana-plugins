83bfb7e76ffc505219207d39287ceb9ba911316c
lynzee9 <lynzee9@hawaii.edu>
2023-04-05 19:30:29 +0000

    Populated nodal awareness table

--- src/nodal-awareness/src/components/TargetChart.tsx
+++ src/nodal-awareness/src/components/TargetChart.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Input } from '@grafana/ui';
+import { PanelData } from '@grafana/data';
 
-export const TargetChart = (props: { width: number; height: number }) => {
+export const TargetChart = (props: { width: number; height: number; data: PanelData }) => {
+  //const { data, width, height } = props;
+  console.log(props.data);
   return (
     <div
       style={{
@@ -11,16 +14,16 @@ export const TargetChart = (props: { width: number; height: number }) => {
         textAlign: 'center',
         display: 'grid',
         gridTemplateRows: 'auto auto auto',
-        gridTemplateColumns: '1fr 3em 3em 3em 3em 3em 3em',
-        // width: props.width,
-        // height: props.height,
+        gridTemplateColumns: '1fr 6em 6em 6em 6em 6em 6em',
+        //width: props.width,
+        //height: props.height,
         overflow: 'scroll',
       }}
     >
       {/** Column labels */}
       <div style={{ fontSize: '0.8em', gridRow: 1, gridColumn: 2 }}>Type</div>
-      <div style={{ fontSize: '0.8em', gridRow: 1, gridColumn: 3 }}>Lat</div>
-      <div style={{ fontSize: '0.8em', gridRow: 1, gridColumn: 4 }}>Lon</div>
+      <div style={{ fontSize: '0.8em', gridRow: 1, gridColumn: 3 }}>Lon</div>
+      <div style={{ fontSize: '0.8em', gridRow: 1, gridColumn: 4 }}>Lat</div>
       <div style={{ fontSize: '0.8em', gridRow: 1, gridColumn: 5 }}>Alt</div>
       <div style={{ fontSize: '0.8em', gridRow: 1, gridColumn: 6 }}>Slant Range</div>
       <div style={{ fontSize: '0.8em', gridRow: 1, gridColumn: 7 }}>Elev</div>
@@ -37,7 +40,7 @@ export const TargetChart = (props: { width: number; height: number }) => {
       {['kauaicc', 'fairbanks', 'surreysc'].map((target, ti) =>
         [0, 1, 2, 3, 4, 5].map((col, ci) => (
           <div key={`na-target-row-${target}-${ti}-${ci}`} style={{ gridRow: ti + 2, gridColumn: ci + 2 }}>
-            <Input type="text" />
+            <Input type="text" value={props.data.series[0].fields[ci].values.get(0)}></Input>
           </div>
         ))
       )}
