fe809d44ac3da7ce0932641d023fe0828680c2be
lynzee-hoegger <lynzeehoegger@gmail.com>
2023-07-03 22:18:02 +0000

    Fixed "fields undefined" error in MED

--- src/mission-events-display/src/SimplePanel.tsx
+++ src/mission-events-display/src/SimplePanel.tsx
@@ -6,6 +6,9 @@ import { MissionEventsDisplay } from './components/MED';
 interface Props extends PanelProps<SimpleOptions> {}
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height, eventBus, timeRange }) => {
+  if (data.series === undefined || data.series.length === 0 || data.series[0].fields === undefined) {
+    return null;
+  }
   return (
     <div style={{ width, height, display: 'flex', flexDirection: 'row' }}>
       <MissionEventsDisplay data={data} width={width} height={height} eventBus={eventBus} timeRange={timeRange} />
